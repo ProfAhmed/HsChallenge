898c79791d269ef1223ac5b8b9c54795bb6b2c6d
DESKTOP-9V79JR0\Osama <ahmed.osama@shgardi.app>
2022-07-23 23:10:35 +0200

    init SignInViewModelTest

---
+++ app/src/test/java/com/hubstaff/challenge/screen/sign_in/SignInViewModelTest.kt
@@ -0,0 +1,25 @@
+package com.hubstaff.challenge.screen.sign_in
+
+import org.junit.Assert.*
+
+import org.junit.Before
+import org.junit.Test
+
+class SignInViewModelTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @Test
+    fun getState() {
+    }
+
+    @Test
+    fun getErrorMsg() {
+    }
+
+    @Test
+    fun login() {
+    }
+}
