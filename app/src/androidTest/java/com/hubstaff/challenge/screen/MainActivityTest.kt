50cd670a26c5fb15a425ef9452c5c66b542504ea
DESKTOP-9V79JR0\Osama <ahmed.osama@shgardi.app>
2022-07-23 23:12:05 +0200

    init MainActivityTest

---
+++ app/src/androidTest/java/com/hubstaff/challenge/screen/MainActivityTest.kt
@@ -0,0 +1,33 @@
+package com.hubstaff.challenge.screen
+
+import org.junit.Assert.*
+
+import org.junit.Before
+import org.junit.Test
+
+class MainActivityTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @Test
+    fun getTimeTicker() {
+    }
+
+    @Test
+    fun setTimeTicker() {
+    }
+
+    @Test
+    fun onCreate() {
+    }
+
+    @Test
+    fun main() {
+    }
+
+    @Test
+    fun defaultPreview() {
+    }
+}
